3da1e2d8d1b7d11bb3f0e81fb0cad5fb16e850cb
lucascencig <cenciglucas@gmail.com>
2023-05-05 23:22:15 -0300

    useRef in useMovies

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { searchMovies } from '../services/movies';
 
 
@@ -8,11 +8,15 @@ export function useMovies({ search }) {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
+  const previousSearch = useRef(search)
 
   const getMovies = async () => {
+    if (search === previousSearch.current) return
+
     try {
       setLoading(true)
       setError(null)
+      previousSearch.current = search
       const newMovies = await searchMovies({ search })
       setMovies(newMovies)
     } catch (e) {
